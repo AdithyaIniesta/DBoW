471759225cbb3d5aa5dcd36a8c09b4fbc3d61d94
Adithya Narasimhaiah Sathish <adithyamaths.1995@gmail.com>
2026-01-22 11:58:30 +0530

    Refactor: Deleted incorrect file and added corner_keypoint.hpp

--- include/dbow/corner_keypoint.hpp
+++ include/dbow/corner_keypoint.hpp
@@ -1,24 +1,29 @@
-/** 
-    * @file keypoint.h 
-    * @brief 2D keypoint for FAST corner detector
-    * Paper Section III: "FAST keypoints are corner-like points" 
- */
+/**
+* @file corner_keypoint.h
+* @brief FAST corner keypoint structure
+* 
+* Paper Section III:
+* "FAST keypoints are corner-like points detected by comparing 
+*  the gray intensity of some pixels in a Bresenham circle of radius 3"
+*/
 
-#ifndef DBOW_KEYPOINT_H
-#include DBOW_KEYPOINT_H
+#ifndef BAG_OF_BINARY_WORDS_CORNER_KEYPOINT_H
+#define BAG_OF_BINARY_WORDS_CORNER_KEYPOINT_H
 
- namespace dbow{
+namespace bag_of_binary_words{
 
-    // A point detected by FAST
-    struct Keypoint{
-        // X position in pixels 
-        float x;
-        // Y position in pixels 
-        float y;
-        // Corner strength score 
-        float response;
-    };
-}; // namespace dbow
+/**
+* @brief FAST corner keypoint
+* 
+* Represents a corner detected by FAST (Features from Accelerated 
+* Segment Test) algorithm as described in paper Section III.
+*/
+struct FastCornerKeypoint {
+    float x_coordinate;      // X position in image (pixels)
+    float y_coordinate;      // Y position in image (pixels)
+    float corner_response;   // Corner strength from FAST detector
+};
 
-#endif
- 
+} // namespace bag_of_binary_words
+
+#endif // BAG_OF_BINARY_WORDS_CORNER_KEYPOINT_H
